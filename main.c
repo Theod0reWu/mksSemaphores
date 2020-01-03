c0397a4543c539d8ab9dfa7b379381df86c5a241
Theod0reWu <twu00@stuy.edu>
2020-01-03 01:17:35 -0500

    this better work

--- main.c
+++ main.c
@@ -23,7 +23,18 @@ int main(int argc, char const *argv[])
 		if (semid == -1){
 			semid = semget(SEM_KEY, 1, 0);
 		}
+    union semun us;
+    us.val = 1;
+    r = semctl(semid, 0 , SETVAL, us);
 
+
+    printf("Creating the file\n");
+    int fd = open("story.txt", O_CREAT | O_TRUNC | O_RDWR, 0644);
+    if (fd == -1) {
+      printf("Error: %s\n", strerror(errno));
+      return -1 ;
+    }
+    close(fd);
 	}
 	else if (strcmp(argv[1], "-r") == 0){
 		printf("Removing the shared memory\n");
@@ -35,15 +46,30 @@ int main(int argc, char const *argv[])
 		}
 		shmctl(shmid, IPC_RMID, 0);
 
-		printf("Remvoing the semaphore\n");
+		printf("Removeing the semaphore\n");
 		int semid = semget(SEM_KEY, 1, 0);
 		if (semid == -1){
 			printf("Uh oh I couldn't get the semaphore\n");
 		}
 		semctl(semid, 0, IPC_RMID);
+
+    int fd = open("story.txt", O_RDONLY);
+    char story[1024];
+    read(fd, story, 20479) ;
+    printf("Here is the story:\n%s\n", story);
+    if (remove(fd) == 0){
+      printf("File Deleted\n");
+    }
+    else{
+      printf("unable to delete file\n");
+    }
 	}
 	else if (strcmp(argv[1], "-v") == 0){
 		printf("The story so far:\n");
+    int fd = open("story.txt", O_RDONLY);
+    char story[1024];
+    read(fd, story, 20479) ;
+    printf("Here is the story:\n%s\n", story);
 	}
 	else{
 		printf("that command is not recognized\n");
---
+++ semaphore.c
@@ -0,0 +1,31 @@
+int main(int argc, char const *argv[])
+{
+	int semid = semget(SEM_KEY, 1, 0);
+	struct sembuf sb;
+	sb.sem_num = 0;
+    sb.sem_op = -1;
+    sb.sem_flg = SEM_UNDO ;
+  	int sem_opp = semop(semd, &sb, 1);
+
+  	int shmid = shmget(SHM_KEY, sizeof (char), 0644);
+  	if (shmid == -1){
+			printf("Well that didn't work. uh oh\n");
+			printf(strerror(errno));
+			printf("%i",errno);
+			return 1;
+	}
+	int * line = shmat(shmid,0,0);
+	printf("Last Line: %s\n", line);
+	char * newLine;
+	printf("New Line: ");
+	fgets(newLine, 1024, stdin);
+	shmdt(line);
+	int fd = open("story.txt", O_CREAT | O_TRUNC | O_RDWR, 0644) ;
+	write(fd, newLine, 1024);
+	close(fd);
+	sb.sem_op = 1;
+	semop(semd, &sb, 1);
+
+
+	return 0;
+}
